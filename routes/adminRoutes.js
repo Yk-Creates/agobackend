169a067200b1383dff3671a4c680f426a857eaeb
Yogesh Vashisth <writetokhair@gmail.com>
2024-08-05 09:21:44 +0530

    change price done

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -4,6 +4,7 @@ import {
   allotDriver,
   AmbulanceOrdersData,
   cabOrdersData,
+  changePrice,
   dashboardData,
   driverAvailableForOrders,
   updateRate,
@@ -21,5 +22,6 @@ router.route("/drivers-cab-available").get(driverAvailableForOrders);
 router.route("/ambulance-orders").get(AmbulanceOrdersData);
 router.route("/drivers").get(allDrivers);
 router.route("/drivers-allot").post(allotDriver);
-router.route("/update-rates").patch(updateRate);
+// router.route("/update-rates").patch(updateRate);
+router.route("/update-price").patch(changePrice);
 export default router;
